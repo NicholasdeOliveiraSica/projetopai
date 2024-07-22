94c7b0062a981ee873bb9399d66fe33db5954d17
NicholasdeOliveiraSica <nick.sica16@gmail.com>
2024-07-22 08:10:09 -0300

    modifiquei o projeto do zero

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const menuOpen = document.querySelector("nav.open")
+const txtMenuOpen = document.querySelector("nav.open a")
+const menuClose = document.querySelector("nav.bloco")
+const focusMask = document.querySelector("div.focus-mask")
+const formMask = document.querySelector("div.form-mask")
+const form = document.querySelector("form.form-fale-conosco")
+
+function menuAbrindo(){
+    menuOpen.style.top = "20px"
+    menuOpen.style.right = "20px"
+    menuClose.style.visibility = "hidden"
+    focusMask.style.display = "block"
+}
+
+function menuFechando(){
+    menuOpen.style.top = "-50%"
+    menuOpen.style.right = "-50%"
+    menuClose.style.visibility = "visible"
+    focusMask.style.display = "none"
+}
+
+function formVem(){
+    form.style.left = "50%"
+    formMask.style.display = "block"
+}
+
+function formVai(){
+    form.style.left = "-150px"
+    formMask.style.display = "none"
+}
