ee7bd95c1f62399f157c3a022d8920c08c23386f
NicholasdeOliveiraSica <nick.sica16@gmail.com>
2024-07-29 21:23:21 -0300

    refiz tudo!!

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const form = document.querySelector(".form")
+const mask = document.querySelector(".focusMask")
+const botaoMenu = document.querySelector(".menu")
+const menu = document.querySelector(".menu-grande")
+
+
+function abreForm() {
+    form.style.left = "50%"   
+    mask.style.display = "block"
+}
+
+function fechaForm() {
+    form.style.left = "-50%"
+    mask.style.display = "none"
+}
+
+function menuAbre() {
+    mask.style.display = "block"
+    botaoMenu.style.display = "none"
+    menu.style.right = "0"
+}
+
+function menuFecha() {
+    mask.style.display = "none"
+    botaoMenu.style.display = "block"
+    menu.style.right = "-250px"
+}
